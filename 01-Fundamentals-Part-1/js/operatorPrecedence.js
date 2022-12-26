421f539f6c488a082e030eb584d2b117b9d011e4
Fernando Canul Caballero <fernandoismaelvirgo@gmail.com>
2022-12-26 14:58:05 -0600

    Update

---
+++ 01-Fundamentals-Part-1/js/operatorPrecedence.js
@@ -0,0 +1,7 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+const now = 2022;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
